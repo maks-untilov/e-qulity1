7f74acc61ecb735e5ae1fcd07865974887e5453e
Maks Untilov <maksimuntilov7@gmail.com>
2023-10-21 02:20:02 +0200

    commit e-quality-bootV03

--- src/main/java/app/equalityboot/controller/InjectController.java
+++ src/main/java/app/equalityboot/controller/InjectController.java
@@ -1,8 +1,10 @@
 package app.equalityboot.controller;
 
+import app.equalityboot.model.Role;
 import app.equalityboot.model.User;
 import app.equalityboot.model.UserWorkDetails;
 import app.equalityboot.service.EmailSenderService;
+import app.equalityboot.service.RoleService;
 import app.equalityboot.service.UserService;
 import app.equalityboot.service.UserWorkDetailsService;
 import app.equalityboot.service.impl.EmailSenderServiceImpl;
@@ -20,17 +22,33 @@ import java.util.List;
 public class InjectController {
     private final EmailSenderServiceImpl emailSenderService;
     private final UserService userService;
+    private final RoleService roleService;
     private final String MY_EMAIL = "maksimuntilov7@gmail.com";
 
-    public InjectController(EmailSenderServiceImpl emailSenderService, UserService userService) {
+    public InjectController(EmailSenderServiceImpl emailSenderService,
+                            UserService userService, RoleService roleService) {
         this.emailSenderService = emailSenderService;
         this.userService = userService;
+        this.roleService = roleService;
     }
 
     @GetMapping
     public String get() throws MessagingException {
         emailSenderService.sendEmail(MY_EMAIL, "Test Subject", "Test Body");
         return "injected";
+    }
+
+    @GetMapping("/roles")
+    public String getAllRoles() {
+        Role user = new Role(Role.RoleName.USER);
+        Role coordinator = new Role(Role.RoleName.MANAGER);
+        Role admin = new Role(Role.RoleName.ADMIN);
+        Role boss = new Role(Role.RoleName.BOSS);
+        roleService.save(user);
+        roleService.save(coordinator);
+        roleService.save(admin);
+        roleService.save(boss);
+        return "injected roles";
     }
 
     @GetMapping("/allow")
